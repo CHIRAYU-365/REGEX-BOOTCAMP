b6a036cbf6f2aa16b2123ba9fb8aa47a2a98cdc2
CHIRAYU-365 <chirayujain362005@gmail.com>
2024-12-01 14:20:54 +0530

    second commit

---
+++ function.js
@@ -0,0 +1,15 @@
+// named function
+function func(s){
+    console.log(s);
+}
+let s= 5;
+func(s);
+
+// named function with return type;
+function sum(a,b){
+    return a+b;
+}
+let a = 4;
+let b = 5;
+let c = sum(a,b);
+console.log(c);
---
+++ script.js
@@ -0,0 +1,6 @@
+//  let var const
+var a ="heelo";
+console.log(a);
+
+var a ="world";
+console.log(a);
